f5c8a12bccad0641113924fc5ea460bb6e839839
Jordan Charest <jordancharest3@gmail.com>
2020-04-19 17:53:59 -0400

    Add skeleton for swim membership protocol, following template in the platform test

---
+++ membership/swim-main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+
+#include "platform/faulty-process.hpp"
+#include "platform/udp-server.hpp"
+
+
+
+int main (int argc, char** argv) {
+    std::shared_ptr<platform::UdpServer> server = std::make_shared<platform::UdpServer>("faulty-process", 9999);
+
+    // encapsulate the entire program in a FaultyProcess
+    // so we can control crashes, message loss, etc.
+    while (true) {
+        std::cout << "Main: starting process" << std::endl;
+        platform::FaultyProcess fp(server);
+        std::thread process_thread([&](){ fp.run(); });
+
+        int i = 0;
+        while (true) {
+            ++i;
+            std::cout << "Main: generating random number " << i << std::endl;
+            // sit and generate random numbers
+            if (i == 10) {
+                std::cout << "Main: crashing process" << std::endl;
+                fp.crash();
+                process_thread.join();
+                break;
+            }
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(1s);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
